92e633d63e4aa9ba1def9b46b75c0241336f0dd9
Charles Zoeller <charles.zoeller1@gmail.com>
2021-01-26 16:24:00 -0700

    added button functionality

--- script.js
+++ script.js
@@ -14,3 +14,113 @@ display next question, update data attributes
 At end of quiz, give option to add initials
 display leaderboard
 */
+var timer;
+var timeCount = 60;
+var currQ = 0;
+var timerEl = document.querySelector(".timer");
+var questionEl = document.querySelector(".question");
+var answersEl = document.querySelector("#answers");
+var responseEl = document.querySelector("#response");
+
+var q1 = {
+  ques: "Which language is responsible for webpage responsiveness?",
+  a1: "CSS",
+  a2: "JS",
+  a3: "HTML",
+  a4: "Java",
+  corr: "ans2",
+};
+
+var q2 = {
+  ques: "Which of the following IS NOT a primitive type in Javascript?",
+  a1: "Number",
+  a2: "Boolean",
+  a3: "String",
+  a4: "Function",
+  corr: "ans4",
+};
+
+var q3 = {
+  ques: "What does CSS stand for?",
+  a1: "Cascading Style Sheet",
+  a2: "Color Style Standard",
+  a3: "Coded Semantic Style",
+  a4: "Corporate Standard Sheet",
+  corr: "ans1",
+};
+
+var q4 = {
+  ques: "Which of the following is not a semantic HTML element?",
+  a1: "header",
+  a2: "div",
+  a3: "main",
+  a4: "aside",
+  corr: "ans2",
+};
+
+var q5 = {
+  ques: "What character is used to denote an array in Javascript?",
+  a1: "{}",
+  a2: "<>",
+  a3: "[]",
+  a4: "()",
+  corr: "ans3",
+};
+
+var questions = [q1, q2, q3, q4, q5];
+
+function setQuestion() {
+  var q = questions[currQ];
+  questionEl.textContent = q.ques;
+  answersEl.children[0].textContent = q.a1;
+  answersEl.children[1].textContent = q.a2;
+  answersEl.children[2].textContent = q.a3;
+  answersEl.children[3].textContent = q.a4;
+}
+
+function init() {
+  setQuestion();
+}
+
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
+
+init();
+
+answersEl.addEventListener("click", function (event) {
+  var element = event.target;
+
+  if (element.matches(".answer")) {
+    var response;
+
+    if (element.id == questions[currQ].corr) {
+      response = "Correct!";
+    } else {
+      response = "Wrong!";
+      timeCount -= 10;
+    }
+    currQ++;
+    responseEl.textContent = response;
+    setQuestion();
+  }
+});
+//console.log(answersEl.children);
