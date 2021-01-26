d8c48bbf767014d71e89b9d3e12f88d6b3ffbff7
Charles Zoeller <charles.zoeller1@gmail.com>
2021-01-26 09:52:34 -0700

    psuedo-code js

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Start button that waits for click to start quiz
+
+Have a timer with one second interval
+update element with current time
+
+Quiz questions and answers held as objects in array, obj also has correct answer
+
+For the quiz questions, give the buttons a 'correct' data attribute
+check target.correct to determine if choice is correct
+If incorrect, deduct time from timer
+display next question, update data attributes
+
+At end of quiz, give option to add initials
+display leaderboard
+*/
